fcedf9bea5aa47a325482b93c57a6f5f72d3b961
schmidtda60742 <schmidtda60742@th-online.de>
2021-11-14 17:29:25 +0100

    updated coordianteTest

--- src/test/java/org/wahlzeit/model/CoordinateTest.java
+++ src/test/java/org/wahlzeit/model/CoordinateTest.java
@@ -1,9 +1,22 @@
 package org.wahlzeit.model;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anyDouble;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
+
+@RunWith(MockitoJUnitRunner.class)
 public class CoordinateTest {
     
     private Coordinate coordinate0;
@@ -13,16 +26,45 @@ public class CoordinateTest {
     private Coordinate coordinateNegative2;
 
     private static double delta = 0;
+    @Mock
+    public ResultSet resultSet;
+
 
     @Before
 	public void setUp() {
 		coordinate0 = new Coordinate(0.0, 0.0, 0.0);
 		coordinate1 = new Coordinate(1.0, 1.0, 1.0);
 		coordinate2 = new Coordinate(1.0, 0.0, 0.0);
-
         coordinateNegative = new Coordinate(-1.0, -2.0, -3.0);
         coordinateNegative2 = new Coordinate(-2.0, -2.0, -3.0);
 
+        try {
+            Mockito.when(resultSet.getDouble("coordinate_x")).thenReturn(5.0);
+            Mockito.when(resultSet.getDouble("coordinate_y")).thenReturn(5.0);
+            Mockito.when(resultSet.getDouble("coordinate_z")).thenReturn(5.0);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        
+
+}
+
+@Test
+public void testReadFrom() throws SQLException {
+    Coordinate coordinate = coordinate0.readFrom(resultSet);
+    assertEquals(5, coordinate.getX(),0);
+    assertEquals(5, coordinate.getY(),0);
+    assertEquals(5, coordinate.getZ(),0);
+
+}
+
+@Test
+public void testWriteOn() throws SQLException {    
+    coordinate0.writeOn(resultSet);
+    verify(resultSet, times(1)).updateDouble(eq("coordinate_x"), anyDouble());
+    verify(resultSet, times(1)).updateDouble(eq("coordinate_y"), anyDouble());
+    verify(resultSet, times(1)).updateDouble(eq("coordinate_z"), anyDouble());   
 }
 
 @Test
@@ -42,6 +84,8 @@ public void testEquals() {
     assertEquals(false, coordinateNegative.equals(new Coordinate(1.0, 2.0, 3.0)));
     
 }
+
+
 
 @Test(expected = IllegalArgumentException.class)
 public void testNullArgument() {
