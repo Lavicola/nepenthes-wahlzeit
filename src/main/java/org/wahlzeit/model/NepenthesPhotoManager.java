8757f3c03a19f05466bb42b122f0cbf4a19affaf
schmidtda60742 <schmidtda60742@th-online.de>
2021-11-14 23:36:57 +0100

    changed NepenthesPhotoManager, NepenthesPhotoManager will now be used

--- src/main/java/org/wahlzeit/model/NepenthesPhotoManager.java
+++ src/main/java/org/wahlzeit/model/NepenthesPhotoManager.java
@@ -1,30 +1,10 @@
 package org.wahlzeit.model;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 public class NepenthesPhotoManager extends PhotoManager {
     
-
-	/**
-	 * 
-	 */
-	protected static final PhotoManager instance = new NepenthesPhotoManager();
-
-
-	/**
-	 * 
-	 */
 	public NepenthesPhotoManager() {
-		photoTagCollector = NepenthesPhotoFactory.getInstance().createPhotoTagCollector();
+		super();
 	}
-
-	/**
-	 * 
-	 */
-	protected Photo createObject(ResultSet rset) throws SQLException {
-		return NepenthesPhotoFactory.getInstance().createPhoto(rset);
-	}
-
 
 }
